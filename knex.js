4d44d9e807e1c4e635ea37ed6a8e91a94029e7d3
Noel Escobedo <noel_ska15@hotmail.com>
2018-03-31 18:41:48 -0600

    Corregimos error en configuracion de knex

--- knex.js
+++ knex.js
@@ -1,4 +1,4 @@
-var knexFile = require('../knexfile.js');
-var knex = require('knex')(knexFile[process.env.NODE_ENV]);
+var knexFile = require('./knexfile.js');
+var knex = require('knex')(knexFile[process.env.NODE_ENV || 'development']);
 
 module.exports = knex;
