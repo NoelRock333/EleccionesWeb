5c64cde0968f92608f9bdbb89b1a4bf6576cae71
Noel Escobedo <noel_ska15@hotmail.com>
2018-03-31 18:42:18 -0600

    Creamos usuarios nuevos

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../knex');
+var SHA256 = require('crypto-js/sha256');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.io.emit("socketToMe", "users");
   res.render('users');
 });
+
+router.get('/signin', function(req, res, next) {
+  res.render('users/signin');
+});
+
+router.post('/signin', function(req, res, next) {
+  var password_hash = SHA256(req.body.password).toString();
+  knex('users')
+    .insert({
+      full_name: req.body.full_name,
+      email: req.body.email,
+      password_hash: password_hash
+    }).then(function(data) {
+      res.send('El usuario ha sido creado');
+    });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('users/login');
+});
+
+router.post('/login', function(req, res, next) {
+  knex('users')
+    .where('email', '=', req.body.email)
+    .first()
+    .then(function(data) {
+      var password_hash = SHA256(req.body.password).toString();
+      if (password_hash == data.password_hash) {
+        res.send('Password correcto');
+      } else {
+        res.send('Password incorrecto');
+      }
+    });
+})
 
 module.exports = router;
