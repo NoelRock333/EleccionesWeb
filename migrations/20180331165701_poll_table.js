42fde81883d4df851f1109ce4a194554b3e042ff
Noel Escobedo <noel_ska15@hotmail.com>
2018-04-02 22:14:53 -0500

    Agregada tabla de municipios

--- migrations/20180331165701_poll_table.js
+++ migrations/20180331165701_poll_table.js
@@ -17,12 +17,20 @@ exports.up = function(knex, Promise) {
             t.string('description');
             t.string('display_name');
         })
+        .createTable('municipalities', function(t) {
+            // Municipios
+            t.increments('id').primary();
+            t.string('identifier');
+            t.string('name');
+        })
         .createTable('poll', function (t) {
             t.increments('id').primary();
             t.string('poll_uuid', 25);
             t.integer('pollster_id').references('users.id');
             t.integer('election_type_id').references('election_types.id'); // [presidente, diputado federal, diputado local, senador]
             t.integer('option_id').references('options.id'); // [morena, pri, pan, prd, coalicion por mexico, mexico al frente, frente comun]
+            t.integer('municipality_id').references('municipalities.id');
+            t.integer('section');
 
             t.timestamps();
         });
@@ -31,6 +39,7 @@ exports.up = function(knex, Promise) {
 exports.down = function(knex, Promise) {
     return knex.schema
         .dropTableIfExists('poll')
+        .dropTableIfExists('municipalities')
         .dropTableIfExists('options')
         .dropTableIfExists('election_types');
 };
